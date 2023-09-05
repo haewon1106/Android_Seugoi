669c9c705ec6cf81d45a01bdac7ba72914fe5017
boram065 <boram4784@gmail.com>
2023-09-05 19:59:46 +0900

    remove titleBar

--- app/src/main/java/com/cookandroid/seugoi/MainActivity.java
+++ app/src/main/java/com/cookandroid/seugoi/MainActivity.java
@@ -1,5 +1,6 @@
 package com.cookandroid.seugoi;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -10,6 +11,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // 타이틀 바 없애기
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
+
     }
 }
 
