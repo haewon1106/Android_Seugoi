bf3667194ae1cb28ee9a338fce788a26d67587de
boram065 <boram4784@gmail.com>
2023-10-01 15:48:12 +0900

    안드로이드 실행하면 노트북 개 느려져서 개 짜증남!!

--- app/src/main/java/com/cookandroid/seugoi/join.java
+++ app/src/main/java/com/cookandroid/seugoi/join.java
@@ -2,14 +2,22 @@ package com.cookandroid.seugoi;
 
 import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class join extends AppCompatActivity {
+    EditText id, emailOne, emailTwo, pw, birth, job;
     @SuppressLint({"MissingInflatedId", "WrongViewCast"})
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.chose_study_img);
+        id = findViewById(R.id.id);
+        emailOne = findViewById(R.id.emailOne);
+        emailTwo = findViewById(R.id.emailTwo);
+        pw = findViewById(R.id.pw);
+        birth = findViewById(R.id.birth);
+        job = findViewById(R.id.job);
     }
 }
